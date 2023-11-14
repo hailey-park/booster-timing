4e14e04e4e69710102eda4e633741239fe9b3e89
Hailey Park <haileypark@berkeley.edu>
2023-11-14 08:52:28 -0800

    added sensitivity analyses

---
+++ analysis/6-sensitivity analysis/delayed-vacc-admin/MAIN SCRIPT (USE THIS).R
@@ -0,0 +1,137 @@
+###################################################################################################
+#Title: Simulation with Non-Severe Infection
+#Author: Hailey Park
+#Date: September 25, 2023
+###################################################################################################
+
+rm(list=ls())
+gc()
+
+setwd("~/Stanford Research/booster-timing") 
+
+#Load libraries
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(tidyr)
+library(tibble)
+library(reshape2)
+library(lubridate)
+library(scales)
+library(foreach)
+library(doParallel)
+library(here)
+library(data.table)
+
+# Set up the number of cores used for parallelization.
+# Use detectCores() to find out how many cores are available.
+num_cores <- detectCores() - 1
+registerDoParallel(num_cores)
+
+#Custom function for combining sim results from parallelization
+comb <- function(...) {
+  mapply('rbind', ..., SIMPLIFY=FALSE)
+}
+
+#Functions for running multiple runs of each booster intervention in parallel
+#NOTE: You can change the number of runs by changing out the 'num_sims'.
+#      In our study, we ran 25 sims for the mean estimates, and 10 sims for each
+#      uncertainty combination.
+
+num_sims <- 25
+
+noBoosterSim_parallel <- function(df){
+  
+  age_info <- df$age_group[1]
+  
+  sim_df <- foreach (i = 1:num_sims, .combine='comb', .multicombine=TRUE) %dopar% {
+    results <- noBoosterSimulation(df)
+    list(results[[1]], results[[2]], results[[3]])
+  }
+  
+  write.csv(sim_df, paste0("results/simulation-results/sensitivity/delayed-vacc-admin/", immune_status, "/waning-", waning, "/sero-", sero, "/case-", case, "/noBooster-", age_info, "-average.csv"))
+}
+
+oneBoosterSim_parallel <- function(df){
+  
+  age_info <- df$age_group[1]
+  
+  sim_df <- foreach (i = 1:num_sims, .combine='comb', .multicombine=TRUE) %dopar% {
+    results <- oneBoosterSimulation(df)
+    list(results[[1]], results[[2]], results[[3]])
+    }
+
+  write.csv(sim_df, paste0("results/simulation-results/sensitivity/delayed-vacc-admin/", immune_status, "/waning-", waning, "/sero-", sero, "/case-", case, "/1Booster-", age_info, "-average.csv"))
+}
+
+annualBoosterSim_parallel <- function(df){
+  
+  age_info <- df$age_group[1]
+  
+  sim_df <- foreach (i = 1:num_sims, .combine='comb', .multicombine=TRUE) %dopar% {
+    results <- annualBoosterSimulation(df)
+    list(results[[1]], results[[2]], results[[3]])
+  }
+  
+  write.csv(sim_df, paste0("results/simulation-results/sensitivity/delayed-vacc-admin/", immune_status, "/waning-", waning, "/sero-", sero, "/case-", case, "/annualBooster-", age_info, "-average.csv"))
+}
+
+biannualBoosterSim_parallel <- function(df){
+  
+  age_info <- df$age_group[1]
+  
+  sim_df <- foreach (i = 1:num_sims, .combine='comb', .multicombine=TRUE) %dopar% {
+    results <- biannualBoosterSimulation(df)
+    list(results[[1]], results[[2]], results[[3]])
+  }
+  
+  write.csv(sim_df, paste0("results/simulation-results/sensitivity/delayed-vacc-admin/", immune_status, "/waning-", waning, "/sero-", sero, "/case-", case, "/biannualBooster-", age_info, "-average.csv"))
+}
+
+
+#Run Booster Interventions
+
+# NOTE:   To quantify uncertainty in the study findings, we generated uncertainty intervals (UI) for our model
+#         estimate. These intervals account for the uncertainty in protective effectiveness and waning over 
+#         time, in addition to the uncertainty in baseline age-specific seroprevalence estimates and age-specific
+#         non-severe infection multipliers. For each model parameter, we created ‘upper’, ‘mean’, and ‘lower’ 
+#         bound versions (Table A5) and ran simulations under each combination of model parameter bounds. Each
+#         combination of model parameters and immune status requires a different calibration, hence why we 
+#         loop through each combination and re-call scripts. 
+#         
+#         
+
+for (immune_status in c("immunocompetent", "immunoMild", "immunoSevere")) {
+  
+for (waning in c("upper", "mean", "lower")){
+  
+  for (sero in c("upper", "mean", "lower")){
+    
+    for (case in c("upper", "mean", "lower")){
+      
+      print(paste0("Immuno Status: ", immune_status))
+      print(paste0("Waning Curve: ", waning))
+      print(paste0("Seroprevalence: ", sero))
+      print(paste0("Case Multipliers: ", case))
+      
+      source(here::here(paste0("analysis/6-sensitivity analysis/delayed-vacc-admin/simulation-data-inputs-95ui-", immune_status, ".R")))
+      source(here::here(paste0("analysis/6-sensitivity analysis/delayed-vacc-admin/intervention-functions-", immune_status, "-delayedVacc.R")))
+
+      set.seed(88)
+      clean_df %>% lapply(average_10_sims_no)
+      
+      set.seed(88)
+      clean_df %>% lapply(average_10_sims_one)
+
+      set.seed(88)
+      clean_df %>% lapply(average_10_sims_annual)
+
+      set.seed(88)
+      clean_df %>% lapply(average_10_sims_biannual)
+      
+      
+    }
+  }
+}
+}
---
+++ analysis/6-sensitivity analysis/delayed-vacc-admin/simulation-data-inputs-95ui-immunocompetent.R
@@ -0,0 +1,70 @@
+###################################################################################################
+#Title: Simulation (data inputs)
+#Author: Hailey Park
+#Date: September 25, 2023
+###################################################################################################
+
+hosp_death_age_stratified <- read.csv("data/clean-data/hosp_death_age_stratified_counts_adj.csv")[,-1]
+
+severe_waning_data <- read.csv("results/waning-predictions/main/severe_waning_predictions_monthly.csv")[,-1]
+nonsevere_waning_data <- read.csv("results/waning-predictions/main/nonsevere_waning_predictions_monthly.csv")[,-1]
+
+
+#MAKE SURE YOU ARE SETTING THE CORRECT WANING CURVE FOR CALIBRATION (mean, lower, upper)
+severe_waning <- severe_waning_data %>% filter(estimate == waning) %>%
+  rowwise() %>% mutate(severe_ve_pred = max(ve_pred, 0))  %>% dplyr::select(-c(estimate, month_input, study, ve_pred))
+nonsevere_waning <- nonsevere_waning_data %>% filter(estimate == waning) %>%
+  rowwise() %>% mutate(nonsevere_ve_pred = max(ve_pred, 0))  %>% dplyr::select(-c(estimate, month_input, ve_pred))
+
+waning_data_clean <- setDT(merge(severe_waning, nonsevere_waning, by = c("age_group", "prior_inf", "months")))
+
+
+#MAKE SURE YOU ARE READING IN THE CORRECT CALIBRATION FILE
+age_18_49 <- read.csv(paste0("results/calibration/immunocompetent/waning-", waning, "/sero-", sero, "/1mil-18-49 years-monthly.csv"))[,-1] 
+age_50_64 <-  read.csv(paste0("results/calibration/immunocompetent/waning-", waning, "/sero-", sero, "/1mil-50-64 years-monthly.csv"))[,-1]
+age_65_74 <-  read.csv(paste0("results/calibration/immunocompetent/waning-", waning, "/sero-", sero, "/1mil-65-74 years-monthly.csv"))[,-1]
+age_75_plus <-  read.csv(paste0("results/calibration/immunocompetent/waning-", waning, "/sero-", sero, "/1mil-75+ years-monthly.csv"))[,-1] 
+############################################################################################
+#clean age matrices
+clean_age_matrix <- function(df){
+  df %>% mutate(months_since_last_dose = as.numeric(as.character(as.factor(interval(time_since_last_dose,as.Date('2022-09-01')) %/% months(1))))) %>% 
+    dplyr::select(c("individual", "age_group", "prior_inf", "months_since_last_dose_inf", "months_since_last_dose", "num_doses","lambda"))
+}
+
+clean_df <- list(age_18_49, age_50_64, age_65_74, age_75_plus) %>%
+  lapply(clean_age_matrix)  
+
+############################################################################################
+#calculate multiplier adjustments
+
+multiplier_adj <- function(df){
+  #age_info <- df$age_group[1]
+  with_protection <- merge(df, waning_data_clean, by.x = c("age_group", "prior_inf", "months_since_last_dose_inf"), 
+                           by.y = c("age_group", "prior_inf", "months"), all.x = TRUE)
+  perfect_immunity_index <- which(with_protection$months_since_last_dose_inf < with_protection$months_since_last_dose & with_protection$prior_inf == 1 & with_protection$months_since_last_dose_inf %in% c(1:3))
+  with_protection[perfect_immunity_index, c("severe_ve_pred", "nonsevere_ve_pred")] <- 1
+  
+  multiplier_adjustment <- (1 - mean(with_protection$nonsevere_ve_pred))/(1 - mean(with_protection$severe_ve_pred))
+  return(multiplier_adjustment)
+}
+
+mult_adj <- clean_df %>%
+  lapply(multiplier_adj)
+
+############################################################################################
+
+if(case == "mean") {
+  case_multipliers <- c(200, 79.6, 22.6, 9.6)
+} else if (case == "lower") {
+  case_multipliers <- c(150, 59.7, 16.95, 7.2)
+} else{
+  case_multipliers <-c(250, 99.5, 28.25, 12)
+}
+
+print(case_multipliers)
+
+nonsevere_infection_multipliers <- data.frame(age_group = c("18-49 years", "50-64 years", "65-74 years", "75+ years"),
+                                              multiplier = case_multipliers,
+                                              multiplier_adjustment = c(mult_adj[[1]], mult_adj[[2]], mult_adj[[3]], mult_adj[[4]])) #FILL IN
+
+
