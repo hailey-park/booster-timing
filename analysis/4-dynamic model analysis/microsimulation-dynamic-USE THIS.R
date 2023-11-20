3225cc303ca761a499441de17b2ef4064470954e
Hailey Park <haileypark@berkeley.edu>
2023-11-20 13:13:02 -0800

    fixed up dynamic

--- analysis/4-dynamic model analysis/microsimulation-dynamic-USE THIS.R
+++ analysis/4-dynamic model analysis/microsimulation-dynamic-USE THIS.R
@@ -32,11 +32,22 @@ registerDoParallel(num_cores)
 #Set number of sims
 num_sims <- 10
 
-#Select either realistic or optimistic vaccine uptake scenario
-set.seed(88)
-assign_vax <- clean_df %>% 
-  lapply(realistic_vax_assignment) 
-# lapply(optimistic_vax_assignment)
+#Set up folder structure to save simulation results
+dir.create("results/simulation-results")
+dir.create("results/simulation-results/dynamic")
+
+for (pop_strat in c("18+", "65+", "75+")) {
+  dir.create(paste0("results/simulation-results/dynamic/", pop_strat, " strategy"))
+  for (vax_assign in c("realistic", "optimistic")) {
+    dir.create(paste0("results/simulation-results/dynamic/", pop_strat, " strategy/", vax_assign))
+    for (intervention in c("1-Booster", "Annual-Booster", "Biannual-Booster")) {
+      dir.create(paste0("results/simulation-results/dynamic/", pop_strat, " strategy/", vax_assign, "/", intervention))
+      for (waning in c("upper", "mean", "lower")) {
+        dir.create(paste0("results/simulation-results/dynamic/", pop_strat, " strategy/", vax_assign, "/", intervention, "/waning-", waning))
+      }
+    }
+  }
+}
 
 
 #Iterate through each population targeting strategy (18+, 65+, 75+)
@@ -48,6 +59,12 @@ for(pop_strat in c("18+", "65+", "75+")) {
     #Read in files 
     source(here::here("analysis/4-dynamic model analysis/simulation-data-inputs-dynamic.R"))
     source(here::here(paste0("analysis/4-dynamic model analysis/intervention-functions-dynamic-", pop_strat, ".R")))
+    
+    #Select either realistic or optimistic vaccine uptake scenario
+    set.seed(88)
+    assign_vax <- clean_df %>% 
+      lapply(realistic_vax_assignment) 
+    # lapply(optimistic_vax_assignment)
     
     #Run one-time booster program
     set.seed(88)
@@ -68,7 +85,7 @@ for(pop_strat in c("18+", "65+", "75+")) {
     }
     
     for (i in c(1:num_sims)){
-      write.csv(sim_df_aB[[i]][[1]], paste0("results/simulation-results/dynamic/", pop_strat, " strategy/realistic/Annual-Booster/lower/", i, ".csv"))
+      write.csv(sim_df_aB[[i]][[1]], paste0("results/simulation-results/dynamic/", pop_strat, " strategy/realistic/Annual-Booster/", waning, "/", i, ".csv"))
     }
     
     #Run semi-annual booster program
@@ -80,7 +97,7 @@ for(pop_strat in c("18+", "65+", "75+")) {
     
     
     for (i in c(1:num_sims)){
-      write.csv(sim_df_bB[[i]][[1]], paste0("results/simulation-results/dynamic/", pop_strat, " strategy/realistic/Biannual-Booster/lower/", i, ".csv"))
+      write.csv(sim_df_bB[[i]][[1]], paste0("results/simulation-results/dynamic/", pop_strat, " strategy/realistic/Biannual-Booster/", waning, "/", i, ".csv"))
     }
     
   }
